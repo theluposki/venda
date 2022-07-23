59cc9446472a2b49897a199cd72f123dda901052
theluposki <theluposki@gmail.com>
2022-07-23 18:04:05 -0300

    add photo

--- views/About.js
+++ views/About.js
@@ -2,12 +2,16 @@ import { database } from "../database/populateData.js"
 export const About = {
   template:
   `
-  <div class="about">
+  <div class="products">
     <ul>
-      <h1>About</h1>
+      <h1>Produtos</h1>
 
       <li v-for="(item, index) in list" :key="index">
-        {{ item }}
+        <div class="i-header">-header</div>
+        <div class="i-main">
+          <img :src="item.urlPhoto" alt="Imagem Produto"/>
+        </div>
+        <div class="i-footer">-footer</div>
       </li>
     </ul>
   </div>
