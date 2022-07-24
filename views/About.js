6c8e36414bccf5491047023dfd9161952ba6bcf1
theluposki <theluposki@gmail.com>
2022-07-23 21:55:37 -0300

    active footer

--- views/About.js
+++ views/About.js
@@ -13,14 +13,34 @@ export const About = {
         <div class="i-main">
           <img :src="item.urlPhoto" alt="Imagem Produto"/>
         </div>
-        <div class="i-footer">-footer</div>
+        <div class="i-footer">
+          <button @click="upDetails(index)" class="btn-chevron">
+            <i v-if="upOrDown" class="ai-chevron-down"></i>
+            <i v-else class="ai-chevron-up"></i>
+          </button>
+        </div>
       </li>
     </ul>
   </div>
   `,
   data() {
     return {
+      upOrDown: false,
       list: database.product
+    }
+  },
+  methods: {
+    upDetails(index) {
+      const items = document.querySelectorAll(".i-footer")
+      this.upOrDown = !this.upOrDown
+      if(this.upOrDown) {
+        console.log("Up", items[index]);
+        items[index].classList.add("active-details")
+        //.active-details
+      } else {
+        items[index].classList.remove("active-details")
+      }
+
     }
   }
 }
